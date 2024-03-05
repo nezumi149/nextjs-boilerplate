af03cc74815284a31c63749dd2126732794ca242
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 14:32:27 -0800

    m

--- app/page.tsx
+++ app/page.tsx
@@ -103,13 +103,13 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <div class="container">
-            <textarea class="story"></textarea>
+          <div className="container">
+            <textarea className="story"></textarea>
             <button id="set-text" type="button">Set text content</button>
             <button id="clear-text" type="button">Clear text content</button>
           </div>
         </a>
       </div>
-    </main>
+    </maino>
   );
 }
