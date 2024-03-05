6b4a306022b6b2780e8bfeea430cac8ce93a1938
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 14:56:13 -0800

    link to p

--- app/page.tsx
+++ app/page.tsx
@@ -91,8 +91,7 @@ export default function Home() {
           </p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <p
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -108,7 +107,7 @@ export default function Home() {
             <button id="set-text" type="button">Set text content</button>
             <button id="clear-text" type="button">Clear text content</button>
           </div>
-        </a>
+        </p>
       </div>
     </main>
   );
