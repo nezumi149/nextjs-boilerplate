da86c90ba5044b591403ffb666eaf501c5efa184
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 22:47:07 -0800

    button

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,25 @@ const Home = () => {
           <button id="set-text" type="button">Set text content</button>
           <button id="clear-text" type="button">Clear text content</button>
         </div>
+        <script>
+          const story = document.body.querySelector(".story");
+          const array = ['a','b','c','d']
+
+          const setText = document.body.querySelector("#set-text");
+          setText.addEventListener("click", () => {
+            
+          const shuffled = array.sort(() => 0.5 - Math.random());
+          let n = 2
+          let random = array.sort(() => .5 - Math.random()).slice(0,n)
+            story.textContent = random.toString();
+          });
+
+          const clearText = document.body.querySelector("#clear-text");
+          clearText.addEventListener("click", () => {
+            story.textContent = "";
+          });
+
+        </script>
     </main>
   )
 }
