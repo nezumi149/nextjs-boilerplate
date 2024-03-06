fcd4f82532a5187bde47a12e752bd5da3ed82fcf
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 19:13:22 -0800

    js2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
+const story = document.body.querySelector(".story");
+const array = ['a','b','c','d']
+
+const setText = document.body.querySelector("#set-text");
+setText.addEventListener("click", () => {
+  
+const shuffled = array.sort(() => 0.5 - Math.random());
+let n = 2
+let random = array.sort(() => .5 - Math.random()).slice(0,n)
+  story.textContent = random.toString();
+});
+
+const clearText = document.body.querySelector("#clear-text");
+clearText.addEventListener("click", () => {
+  story.textContent = "";
+});
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -105,24 +122,6 @@ export default function Home() {
             <button id="set-text" type="button">Set text content</button>
             <button id="clear-text" type="button">Clear text content</button>
           </div>
-          <script>
-const story = document.body.querySelector(".story");
-const array = ['a','b','c','d']
-
-const setText = document.body.querySelector("#set-text");
-setText.addEventListener("click", () => {
-  
-const shuffled = array.sort(() => 0.5 - Math.random());
-let n = 2
-let random = array.sort(() => .5 - Math.random()).slice(0,n)
-  story.textContent = random.toString();
-});
-
-const clearText = document.body.querySelector("#clear-text");
-clearText.addEventListener("click", () => {
-  story.textContent = "";
-});
-</script>
         </p>
       </div>
     </main>
