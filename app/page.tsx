19b6c7154ebb085a54fb56d67d50989b68e873bd
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 22:58:16 -0800

    m

--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,23 @@ const Home = () => {
   )
 }
 
-const story = document.body.querySelector(".story");
-const array = ['a','b','c','d']
-
-const setText = document.body.querySelector("#set-text");
-setText!.addEventListener("click", () => {
+if (process.browser) {
+  const story = document.body.querySelector(".story");
+  const array = ['a','b','c','d']
   
-const shuffled = array.sort(() => 0.5 - Math.random());
-let n = 2
-let random = array.sort(() => .5 - Math.random()).slice(0,n)
-  story!.textContent = random.toString();
-});
-
-const clearText = document.body.querySelector("#clear-text");
-clearText!.addEventListener("click", () => {
-  story!.textContent = "";
-});
+  const setText = document.body.querySelector("#set-text");
+  setText!.addEventListener("click", () => {
+    
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  let n = 2
+  let random = array.sort(() => .5 - Math.random()).slice(0,n)
+    story!.textContent = random.toString();
+  });
+  
+  const clearText = document.body.querySelector("#clear-text");
+  clearText!.addEventListener("click", () => {
+    story!.textContent = "";
+  });
+}
 
 export default Home;
