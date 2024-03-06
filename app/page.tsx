c604b871299888a85a1a64e85838bbdc1c35a2c3
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 23:58:35 -0800

    button2

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,20 @@
+function SetTextButton(){
+  return (
+    <button id="set-text" type="button">Set text content</button>
+  );
+}
+
 const Home = () => {
 
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <textarea className="story"></textarea>
-          <button id="set-text" type="button">Set text content</button>
+          <SetTextButton />
           <button id="clear-text" type="button">Clear text content</button>
         </div>
     </main>
   )
-}
-
-if (process.browser) {
-  const story = document.body.querySelector(".story");
-  const array = ['a','b','c','d']
-  
-  const setText = document.body.querySelector("#set-text");
-  setText!.addEventListener("click", () => {
-    
-  const shuffled = array.sort(() => 0.5 - Math.random());
-  let n = 2
-  let random = array.sort(() => .5 - Math.random()).slice(0,n)
-    story!.textContent = random.toString();
-  });
-  
-  const clearText = document.body.querySelector("#clear-text");
-  clearText!.addEventListener("click", () => {
-    story!.textContent = "";
-  });
 }
 
 export default Home;
