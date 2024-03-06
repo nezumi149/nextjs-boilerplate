a377ee877a131a63696043a7cddfd4a503bf9cd2
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-05 22:31:32 -0800

    input1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 const Home = () => {
 
   return(
-    <div className="container">
-      <textarea className="story"></textarea>
-      <button id="set-text" type="button">Set text content</button>
-      <button id="clear-text" type="button">Clear text content</button>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <textarea className="story"></textarea>
+          <button id="set-text" type="button">Set text content</button>
+          <button id="clear-text" type="button">Clear text content</button>
+        </div>
+    </main>
   )
 }
 
