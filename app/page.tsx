39e767ea31ee04fea0d1161e0fcd0bd5cba3b699
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-06 19:57:12 -0800

    clicks

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,35 @@
 import { useState } from 'react';
 
-function Button({title, id}: {title:any, id: any}){
+function Button({title, id, onclick}: {title:any, id: string, onclick: any}){
   return (
-    <button id={id} type="button">{title ? title : 'Button Name'}</button>
+    <button onClick={onclick}  id={id} type="button">{title ? title : 'Button Name'}</button>
+  );
+}
+
+function TextArea({text, id}: {text:string, id:string}){
+  return(
+    <textarea id={id}>{text}</textarea>
   );
 }
 
 const Home = () => {
 
+  const [text, setText] = useState("");
+
+  function setTextArea({text} : {text: string}){
+    setText(text);
+  }
+
+  function clearTextArea(){
+    setText("");
+  }
+
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <textarea className="story"></textarea>
-          <Button title="Set text context" id="set-text"/>
-          <Button title="Clear text context" id="clear-text"/>
+          <TextArea text={text}} id="textarea"/>
+          <Button title="Set text context" id="set-text" onclick={setTextArea} />
+          <Button title="Clear text context" id="clear-text" onclick={clearTextArea}/>
         </div>
     </main>
   )
