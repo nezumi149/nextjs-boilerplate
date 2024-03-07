ea89d0b3e14055601f161b0d7b66352982536e9a
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-07 02:19:32 -0800

    clear text

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,6 @@ import { useState } from 'react';
 const Home = () => {
   const [textbox, setText] = useState("");
 
-  function clickClear(){
-    setText("");
-  }
-
-  function clickSetText(){
-    setText("default text");
-    console.log(textbox);
-  }
-
   function updateText(value: string){
     setText(value);
   }
@@ -21,12 +12,9 @@ const Home = () => {
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <textarea defaultValue="" onChange={e => updateText(e.target.value)}></textarea>
-          <button onClick={clickClear}>
+          <textarea value={textbox} onChange={e => updateText(e.target.value)}></textarea>
+          <button onClick={() => setText("")}>
             Clear Text
-          </button>
-          <button onClick={clickSetText}>
-            Set Text
           </button>
         </div>
     </main>
