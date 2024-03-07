f1bd224f7e38e8867a009166bcb0324a870921fa
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-06 18:35:42 -0800

    react?

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
+
 function Button({title, id}: {title:any, id: any}){
   return (
-    <button id={id} type="button">{title}</button>
+    <button id={id} type="button">{title ? title : 'Button Name'}</button>
   );
 }
 
@@ -10,8 +12,8 @@ const Home = () => {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           <textarea className="story"></textarea>
-          <Button title="Set text context" id="button-1"/>
-          <Button title="Clear text context" id="button-2"/>
+          <Button title="Set text context" id="set-text"/>
+          <Button title="Clear text context" id="clear-text"/>
         </div>
     </main>
   )
