aba97955511f58a2cb94c1976bd71352b3d3efbe
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-07 01:36:15 -0800

    update text?

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,6 @@
 
 import { useState } from 'react';
 
-function Button({title, id}: {title:any, id: string}){
-  return (
-    <button id={id} type="button">{title ? title : 'Button Name'}</button>
-  );
-}
-
-function TextArea({text, id}: {text:string, id:string}){
-  return(
-    <textarea id={id}>{text}</textarea>
-  );
-}
-
 const Home = () => {
   const [textbox, setText] = useState("");
 
@@ -29,7 +17,7 @@ const Home = () => {
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <textarea>{textbox}</textarea>
+          <textarea value={textbox}></textarea>
           <button onClick={clickClear}>
             Clear Text
           </button>
