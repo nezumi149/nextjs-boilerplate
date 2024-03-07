b1c2320566cec0b580304bd9a0196c920e543f93
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-07 01:42:48 -0800

    default value 2

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,14 @@ const Home = () => {
     console.log(textbox);
   }
 
+  function updateText({value}:{value: any}){
+    setText(value);
+  }
+
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <textarea value={textbox}></textarea>
+          <textarea defaultValue="" onChange={e => updateText(e.target.value)}></textarea>
           <button onClick={clickClear}>
             Clear Text
           </button>
