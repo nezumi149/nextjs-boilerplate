cd39dda1cbb99ae6caaa32240cd34391d6635599
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-07 16:43:56 -0800

    m

--- app/page.tsx
+++ app/page.tsx
@@ -20,15 +20,13 @@ const Home = () => {
           <Input text={textC} setText={setTextC} />
           <Input text={textD} setText={setTextD} />
           <p>
-            The current state is: <br>
-            {textA} <br>{textB} <br>{textC} <br>{textD} <br>
+            The current state is:
           </p>
         </div>
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-
+            Word samples list: {wordSample}
           </p>
-          Word samples list: {wordSample}
         </div>
     </main>
   )
