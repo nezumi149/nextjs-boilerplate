0254855e5e5a3012d63038cd2998ff34b3f32a5f
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-08 13:18:58 -0800

    camel case for css styles

--- app/Leaf.js
+++ app/Leaf.js
@@ -7,13 +7,13 @@ const Leaf = (props) => {
     return (
        <div>
         <div style={{ height:'200px', width:'200px'}} onClick={() => setRotation(rotation + (props.disabled?1:0))}>
-            <p style={{ position: 'absolute', top:'0%', width:'100%', text-align:'center'}}>
+            <p style={{ position: 'absolute', top:'0%', width:'100%', textAlign:'center'}}>
                 {props.words[(0+rotation)%4]}
             </p>
             <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
                 {props.words[(1+rotation)%4]}
             </p>
-            <p style={{ position: 'absolute', top:'100%', width:'100%', text-align:'center'}}>
+            <p style={{ position: 'absolute', top:'100%', width:'100%', textAlign:'center'}}>
                 {props.words[(2+rotation)%4]}
             </p>
             <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
