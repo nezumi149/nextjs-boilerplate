d1d650aac726f178ab5e9d460bae2f65bb08845c
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-08 13:01:47 -0800

    update leaf style

--- app/Leaf.js
+++ app/Leaf.js
@@ -5,11 +5,17 @@ const Leaf = (props) => {
     const [rotation, setRotation] = useState(0);
   
     return (
-      <div>
-        <p onClick={() => if(props.disabled){setRotation(rotation + 1)}}>
-            {props.words[(0+rotation)%4]}<br />
-            {props.words[(1+rotation)%4]}<br />
-            {props.words[(2+rotation)%4]}<br />
+      <div style={{ height:'200px' width:'200px'}} onClick={() => if(props.disabled){setRotation(rotation + 1)}}>
+        <p style={{ position: 'absolute', top:'0%', width:'100%', text-align:'center'}}>
+            {props.words[(0+rotation)%4]}
+        </p>
+        <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
+            {props.words[(1+rotation)%4]}
+        </p>
+        <p style={{ position: 'absolute', top:'100%', width:'100%', text-align:'center'}}>
+            {props.words[(2+rotation)%4]}
+        </p>
+        <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
             {props.words[(3+rotation)%4]}
         </p>
       </div>
