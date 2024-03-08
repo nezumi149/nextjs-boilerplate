974e7203285c3bb1ba5f191c4f306a6eab301f36
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-08 13:14:34 -0800

    attempt syntax fix on rotation

--- app/Leaf.js
+++ app/Leaf.js
@@ -5,19 +5,21 @@ const Leaf = (props) => {
     const [rotation, setRotation] = useState(0);
   
     return (
-      <div style={{ height:'200px', width:'200px'}} onClick={() => if(props.disabled){setRotation(rotation + 1)}}>
-        <p style={{ position: 'absolute', top:'0%', width:'100%', text-align:'center'}}>
-            {props.words[(0+rotation)%4]}
-        </p>
-        <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
-            {props.words[(1+rotation)%4]}
-        </p>
-        <p style={{ position: 'absolute', top:'100%', width:'100%', text-align:'center'}}>
-            {props.words[(2+rotation)%4]}
-        </p>
-        <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
-            {props.words[(3+rotation)%4]}
-        </p>
+       <div>
+        <div style={{ height:'200px', width:'200px'}} onClick={() => setRotation(rotation + (props.disabled?1:0))}>
+            <p style={{ position: 'absolute', top:'0%', width:'100%', text-align:'center'}}>
+                {props.words[(0+rotation)%4]}
+            </p>
+            <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
+                {props.words[(1+rotation)%4]}
+            </p>
+            <p style={{ position: 'absolute', top:'100%', width:'100%', text-align:'center'}}>
+                {props.words[(2+rotation)%4]}
+            </p>
+            <p style={{ position: 'absolute', top:'50%', right: '15px'}}>
+                {props.words[(3+rotation)%4]}
+            </p>
+        </div>
       </div>
     );
   };
