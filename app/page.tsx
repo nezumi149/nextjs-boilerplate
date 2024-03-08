1d597a1aa310d2ed1de2337274137d96d416a7e1
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-07 16:23:07 -0800

    m

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <Input text={text} setText={setText} />
+          <Input text={textbox} setText={setText} />
           <button onClick={() => setText("")}>
             Clear Text 
           </button>
