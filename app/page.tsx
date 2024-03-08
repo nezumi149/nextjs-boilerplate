5b848be66737c40424d99adddc4392341ffece0c
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-08 15:22:59 -0800

    try new positions

--- app/page.tsx
+++ app/page.tsx
@@ -15,15 +15,20 @@ const Home = () => {
   const [disabled, setDisabled] = useState(false);
 
   return(
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridGap: 20}} className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <div id='fourLeafOuter' style={{ fontSize: 60, fontFamily: 'aga-arabesque', color: 'gray'}}>
-            <Input text={textA} setText={setTextA} disabled={disabled}/>
-            <div id='fourLeafInner'style={{ height:'339px', width:'339px', position:'relative'}}>
-            <Leaf words={wordSample.slice(0,4)}  disabled={disabled} style={{ position: 'absolute', top:0, left: 0}}/>
-            <Leaf words={wordSample.slice(4,8)}  disabled={disabled} style={{ position: 'absolute', top:0, right: 0}}/>
-            <Leaf words={wordSample.slice(8,12)}  disabled={disabled} style={{ position: 'absolute', bottom:0, right: 0}}/>
-            <Leaf words={wordSample.slice(12,16)}  disabled={disabled} style={{ position: 'absolute', bottom:0, left: 0}}/> 
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"><div id='fourLeafOuter' style={{ fontSize: 20, fontFamily: 'aga-arabesque', color: 'gray'}}>
+        <Input text={textA} setText={setTextA} disabled={disabled}/>
+          <div id='fourLeafInner'style={{ height:'360px', width:'360px', position:'relative'}}>
+            <div style={{height:'179px', width:'179px', position: 'absolute', top:0, left: 0}}>
+              <Leaf words={wordSample.slice(0,4)}  disabled={disabled}/>
+            </div>
+            <div style={{height:'179px', width:'179px', position: 'absolute', top:0, right: 0}}>
+              <Leaf words={wordSample.slice(4,8)}  disabled={disabled}/>
+            </div>
+            <div style={{height:'179px', width:'179px', position: 'absolute', bottom:0, right: 0}}>
+              <Leaf words={wordSample.slice(8,12)} disabled={disabled}/>
+            </div>
+            <div style={{height:'179px', width:'179px', position: 'absolute', bottom:0, left: 0}}>
+              <Leaf words={wordSample.slice(12,16)} disabled={disabled}/>
             </div>
           </div>
         </div>
