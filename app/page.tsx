87255426302248cc8facc20ca2656ad75890453f
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-08 16:01:06 -0800

    enable text rotation

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,14 @@ const Home = () => {
   const [textB, setTextB] = useState("");
   const [textC, setTextC] = useState("");
   const [textD, setTextD] = useState("");
+  const [rotation, setRotation] = useState(0); //rotation should be between 0 and 3 inclusive
   const [disabled, setDisabled] = useState(false);
+  const textsRotation = [textA,textB,textC,textD];
+  const setterRotation = [setTextA,setTextB,setTextC,setTextD]
 
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24"><div id='fourLeafOuter' style={{ fontSize: 20, fontFamily: 'aga-arabesque', color: 'gray'}}>
-        <Input text={textA} setText={setTextA} disabled={disabled}/>
+        <Input text={textsRotation[rotation]} setText={setterRotation[rotation]} disabled={disabled}/>
           <div id='fourLeafInner'style={{ height:'360px', width:'360px', position:'relative'}}>
             <div style={{height:'179px', width:'179px', position: 'absolute', top:0, left: 0}}>
               <Leaf words={wordSample.slice(0,4)}  disabled={disabled}/>
@@ -35,6 +38,9 @@ const Home = () => {
         <button onClick={() => setDisabled(!disabled)}>
           {disabled ? "Unlock" : "Submit"}
         </button>
+        <p>
+          {textA}<br />{textB}<br />{textC}<br />{textD}<br />
+        </p>
     </main>
   )
 }
