761ea6254a4867006d1c7c327325690d444d476b
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-09 18:26:09 -0800

    fix syntax

--- app/Input.js
+++ app/Input.js
@@ -2,19 +2,13 @@ const inputStyle={
   textAlign: 'center'
 }
 
-const divStyle = {
-  width:'249px',
-  margin: 'auto',
-  paddingBottom: '10px'
-}
-
 const Input = (props) => {
     const handleChange = (e) => {
       props.setText(e.target.value);
     };
   
     return (
-      <div style={divStyle}>
+      <div style={props.style}>
         <input style={inputStyle} value={props.text} onChange={handleChange} disabled={props.disabled}/>
       </div>
     );
