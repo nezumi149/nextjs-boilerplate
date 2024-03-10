23e0bed4639efeab6efb53f60f3b94fca021ed52
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-09 18:41:14 -0800

    update typo for all styling

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const Home = () => {
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id='fourLeafOuter' style={{ fontSize: 20, fontFamily: 'aga-arabesque', color: 'gray', height: '440px', width: '440px'}}>
-        <Input text={textsRotation[rotation]} setText={setterRotation[rotation]} disabled={disabled} style={{greyInputStyle}}/>
+        <Input text={textsRotation[rotation]} setText={setterRotation[rotation]} disabled={disabled} styling={{greyInputStyle}}/>
         <Input text={textsRotation[(rotation + 3) % 4]} setText={setterRotation[rotation]} disabled='true' styling={{rightInputStyle, greyInputStyle}}/>
         <Input text={textsRotation[(rotation + 2) % 4]} setText={setterRotation[rotation]} disabled='true' styling={{bottomInputStyle, greyInputStyle}}/>
         <Input text={textsRotation[(rotation + 1) % 4]} setText={setterRotation[rotation]} disabled='true' styling={{leftInputStyle, greyInputStyle}}/>
