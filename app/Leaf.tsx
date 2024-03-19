c222ae9c03f98c08e8728ea98c52f068f3e582c9
nezumi149 <81207644+nezumi149@users.noreply.github.com>
2024-03-19 01:14:50 -0700

    fix leaf (return to 4 words)

--- app/Leaf.tsx
+++ app/Leaf.tsx
@@ -32,7 +32,22 @@ const Leaf = forwardRef<HTMLDivElement, LeafProps>(({ id, withOpacity, isDraggin
         ...style,
     };
 
-    return <div ref={ref} style={inlineStyles} {...props} onClick={() => setRotation(rotation + (props.disabled?1:0))}>{id}</div>;
+    return (
+        <div ref={ref} style={inlineStyles} {...props} onClick={() => setRotation(rotation + (props.disabled?1:0))}>
+            <p style={{ display:'inline-block', position: 'absolute', top:'0%', width:'100%', textAlign:'center'}}>
+                {props.words[(0+rotation)%4]}
+            </p>
+            <p style={{ display:'inline-block', position: 'absolute', top:'50%', right: '0%', writingMode: 'vertical-lr', transform: 'translateY(-50%)'}}>
+                {props.words[(1+rotation)%4]}
+            </p>
+            <p style={{ display:'inline-block', position: 'absolute', bottom:'0%', width:'100%', textAlign:'center',  rotate: '180deg'}}>
+                {props.words[(2+rotation)%4]}
+            </p>
+            <p style={{ display:'inline-block', position: 'absolute', top:'50%', left: '0%', writingMode: 'vertical-lr',  transform: 'translateY(-50%) scale(-1)'}}>
+                {props.words[(3+rotation)%4]}
+            </p>
+        </div>
+    );
 });
 
 export default Leaf;
